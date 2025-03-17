fed8eb07c8574e4ffe3935bae6947e7b21a188b6
bojokoeva <bojokoeva34@gmail.com>
2025-03-16 22:32:41 -0400

    I added comment #2

--- cc_15.js
+++ cc_15.js
@@ -1,3 +1,3 @@
 // Task 1: Base Structure Setup
-const riskDashboard = document.getElementById("riskDashboard");
+const riskDashboard = document.getElementById("riskDashboard"); // Get the riskDashboard container where risk items will be added
 console.log("Risk Dashboard Loaded");
