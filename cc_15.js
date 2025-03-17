af0df0ef889a2f148913826732287551e553f6c7
bojokoeva <bojokoeva34@gmail.com>
2025-03-16 22:35:47 -0400

    Task 2 - Adding Risk Items.

--- cc_15.js
+++ cc_15.js
@@ -1,3 +1,8 @@
 // Task 1: Base Structure Setup
 const riskDashboard = document.getElementById("riskDashboard"); // Get the riskDashboard container where risk items will be added
 console.log("Risk Dashboard Loaded");
+
+// Task 2: Adding Risk Items Dynamically
+function addRiskItem(riskName, riskLevel, department) {  //Function to add a new risk item to the dashboard
+    const riskCard = document.createElement("div");  //Create a new div element for the risk card
+    riskCard.className = "riskCard";  // Assign a class for styling the risk card
